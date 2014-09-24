2842d856207704d42e2caf22edfa95fc143b1a57
JEroen Bosch <jeroon.bosch@gmail.com>
2014-09-24 14:06:13 +0200

    aaaaaa

--- Deeltoets1/static/js/app.js
+++ Deeltoets1/static/js/app.js
@@ -2,10 +2,10 @@ var Dt1 = Dt1||{}; //namespace
 
 (function() { //self-invoking anonymous function
 
-	Dt1.controller = {
+	Dt1.controller = { //controllers
 		init : function (){
-			Dt1.router.init();
-            Dt1.section.init();
+			Dt1.router.init(); //initialize routie
+            Dt1.section.init(); //initialize transparency
 		}
 	};
 
@@ -25,7 +25,7 @@ var Dt1 = Dt1||{}; //namespace
         }
     };
 
-    Dt1.content = {
+    Dt1.content = { //titel, releaseDate, etc. verwijzen naar HTML 'data-bind'
         about : {
             titel: "About this app",
             //description: "Cities fall but they are rebuilt. heroes die but they are remembered. the man likes to play chess; let's get him some rocks. circumstances have taught me that a man's ethics are the only possessions he will take beyond the grave. multiply your anger by about a hundred, kate, that's how much he thinks he loves you. bruce... i'm god. multiply your anger by about a hundred, kate, that's how much he thinks he loves you. no, this is mount everest. you should flip on the discovery channel from time to time. but i guess you can't now, being dead and all. rehabilitated? well, now let me see. you know, i don't have any idea what that means. mister wayne, if you don't want to tell me exactly what you're doing, when i'm asked, i don't have to lie. but don't think of me as an idiot. rehabilitated? well, now let me see. you know, i don't have any idea what that means. cities fall but they are rebuilt. heroes die but they are remembered. no, this is mount everest. you should flip on the discovery channel from time to time. but i guess you can't now, being dead and all. /n I did the same thing to gandhi, he didn't eat for three weeks. bruce... i'm god. cities fall but they are rebuilt. heroes die but they are remembered. i once heard a wise man say there are no perfect men. only perfect intentions. cities fall but they are rebuilt. heroes die but they are remembered. boxing is about respect. getting it for yourself, and taking it away from the other guy. well, what is it today? more spelunking? /n let me tell you something my friend. hope is a dangerous thing. hope can drive a man insane. bruce... i'm god. well, what is it today? more spelunking? it only took me six days. same time it took the lord to make the world. i did the same thing to gandhi, he didn't eat for three weeks. Let me tell you something my friend. hope is a dangerous thing. hope can drive a man insane. boxing is about respect. getting it for yourself, and taking it away from the other guy. mister wayne, if you don't want to tell me exactly what you're doing, when i'm asked, i don't have to lie. but don't think of me as an idiot. you measure yourself by the people who measure themselves by you. circumstances have taught me that a man's ethics are the only possessions he will take beyond the grave. circumstances have taught me that a man's ethics are the only possessions he will take beyond the grave. you measure yourself by the people who measure themselves by you. you measure yourself by the people who measure themselves by you. /n that tall drink of water with the silver spoon up his ass. i once heard a wise man say there are no perfect men. only perfect intentions. mister wayne, if you don't want to tell me exactly what you're doing, when i'm asked, i don't have to lie. but don't think of me as an idiot. boxing is about respect. getting it for yourself, and taking it away from the other guy. That tall drink of water with the silver spoon up his ass. well, what is it today? more spelunking? i now issue a new commandment: thou shalt do the dance. let me tell you something my friend. hope is a dangerous thing. hope can drive a man insane. i did the same thing to gandhi, he didn't eat for three weeks. the man likes to play chess; let's get him some rocks. i now issue a new commandment: thou shalt do the dance. i now issue a new commandment: thou shalt do the dance. multiply your anger by about a hundred, kate, that's how much he thinks he loves you. i don't think they tried to market it to the billionaire, spelunking, base-jumping crowd. that tall drink of water with the silver spoon up his ass. it only took me six days. same time it took the lord to make the world."
@@ -60,7 +60,7 @@ var Dt1 = Dt1||{}; //namespace
         directives: {
             cover: {
                 src: function () {
-                    return this.cover;
+                    return this.cover; //verwijst naar URL in de Dt1.content.movies.cover
                 }
             }
         }
@@ -68,14 +68,15 @@ var Dt1 = Dt1||{}; //namespace
 
     Dt1.section = {
         init: function () {
+            document.getElementById("about").classList.add('active'); //eerst 'about' als active setten
             Dt1.section.about();
             Dt1.section.movies();
         },
         about: function () {
-            Transparency.render(document.getElementById("about"), Dt1.content.about);
+            Transparency.render(document.getElementById("about"), Dt1.content.about); //rendert de HTML section 'about'
         },
         movies: function () {
-            Transparency.render(document.getElementById("movies"), Dt1.content.movies, Dt1.content.directives);
+            Transparency.render(document.getElementById("movies"), Dt1.content.movies, Dt1.content.directives); //rendert de HTML section 'movies' en directives
         },
         toggle: function (section) {
             var sections = document.querySelectorAll('section');
@@ -84,10 +85,9 @@ var Dt1 = Dt1||{}; //namespace
                 sections[i].classList.remove('active');
             }
 
-            document.getElementById(section).classList.add('active');
+            document.getElementById(section).classList.add('active'); //voegt een .active class aan de sections toe, wanneer deze geselecteerd is
         }
     };
 
-    Dt1.controller.init();
-
+    Dt1.controller.init(); //uitvoeren van de controller, om ook daadwerkelijk iets te doen
 })();
